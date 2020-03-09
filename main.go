90b5619b948098f187c1e8b6131785b0c615493f
Ian S. Pringle <ipringle2@liberty.edu>
2020-03-09 11:58:11 -0400

    Removed a lot of excess
    - Still not working
    - Trying to use the TextToMetricFamilies method

--- main.go
+++ main.go
@@ -3,88 +3,56 @@ package main
 import (
 	"flag"
 	"fmt"
-	dto "github.com/prometheus/client_model/go"
 	"github.com/prometheus/common/expfmt"
 	"io"
 	//"io/ioutil"
+	"log"
 	"net/http"
 	"time"
 )
 
-func get_metrics(url string) (io.Reader, error) {
+func get_metrics(url string) io.Reader {
 	timeout := time.Duration(60 * time.Second)
 	client := &http.Client{
 		Timeout: timeout,
 	}
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
+	req, newReqErr := http.NewRequest("GET", url, nil)
+	if newReqErr != nil {
+		log.Fatal("http.NewRequest error:", newReqErr)
 	}
 
 	res, getErr := client.Do(req)
-	if getErr != nil {
-		return nil, getErr
-	}
 	defer res.Body.Close()
+	if getErr != nil {
+		log.Fatal("client.Do error:", getErr)
+	}
 
 	//body, readErr := ioutil.ReadAll(res.Body)
 	//if readErr != nil {
-	//	return nil, readErr
+	//	log.Fatal("ioutil.ReadAll error:", readErr)
 	//}
-	//fmt.Println(body)
+	//fmt.Println(string(body))
 
-	return res.Body, nil
+	return res.Body
 }
 
-func parseMetrics(raw io.Reader) (expfmt.Decoder, error) {
-	return expfmt.NewDecoder(raw, "text"), nil
-}
-
-func parseMetricsAlso(raw io.Reader) (map[string]*dto.MetricFamily, error) {
-	var parser expfmt.TextParser
-	metrics, metricErr := parser.TextToMetricFamilies(raw)
-	if metricErr != nil {
-		return nil, metricErr
-	}
-	return metrics, nil
-}
-
-func retriveAddr() (string, error) {
+func retriveAddr() string {
 	proto := flag.String("proto", "http", "specify protocol used for scrape")
 	host := flag.String("host", "localhost", "specify the host to scrape")
 	port := flag.String("port", "9100", "specify the port to scrape")
 	flag.Parse()
-	addr := fmt.Sprintf("%s://%s:%s", *proto, *host, *port)
-	return addr, nil
+	addr := fmt.Sprintf("%s://%s:%s/metrics", *proto, *host, *port)
+	return addr
 }
 
 func main() {
-	addr, flagErr := retriveAddr()
-	if flagErr != nil {
-		fmt.Println(flagErr)
-	}
-
-	rawMetrics, err := get_metrics(addr)
+	addr := retriveAddr()
+	rawMetrics := get_metrics(addr)
+	parser := expfmt.TextParser{}
+	metrics, err := parser.TextToMetricFamilies(rawMetrics)
 	if err != nil {
-		fmt.Println(err)
-	}
-
-	metrics := dto.MetricFamily{}
-	parser, parseErr := parseMetrics(rawMetrics)
-	if parseErr != nil {
-		fmt.Println(parseErr)
-	}
-
-	decodeErr := parser.Decode(&metrics)
-	if decodeErr != nil {
-		fmt.Println(decodeErr)
+		log.Fatal("parser.TextToMetricFamilies error:", err)
 	}
 	fmt.Println(metrics)
-
-	textMetrics, parseErr := parseMetricsAlso(rawMetrics)
-	if parseErr != nil {
-		fmt.Println(parseErr)
-	}
-	fmt.Println(textMetrics)
 }
